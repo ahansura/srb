3e16bf922ee92bdb9b13c3b141e2ae460ce7a0e1
ahansura <98645057+ahansura@users.noreply.github.com>
2022-01-29 17:29:19 +0530

    Update rap.sh

--- rap.sh
+++ rap.sh
@@ -3,4 +3,4 @@
 #This is an example you can edit and use
 #There are numerous parameters you can set, please check Help and Examples folder
 
-./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool stratum+tcps://eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
+./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool eu.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb --tls true
