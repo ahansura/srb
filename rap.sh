9dfbc013be814448cd1bb5bff30702dbbbaa533a
ahansura <98645057+ahansura@users.noreply.github.com>
2022-01-29 17:02:17 +0530

    Create rap.sh

---
+++ rap.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool us-west.flockpool.com:5555 --wallet RFtgkDCsn41AwwU3ZuBEjFszNMZRw227hb
